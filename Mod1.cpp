ddcbfc1dd02a2be7b3dae5f26f183b60eae5bdeb
EbrahimElTehewy <ebrahemeltehewy@gmail.com>
2026-04-20 22:24:50 +0200

    add material, csvHandler classes

--- Mod1.cpp
+++ Mod1.cpp
@@ -6,93 +6,99 @@
 
 using namespace std;
 
-class material {
+
+// Class to handle CSV files operations
+class csvHandler 
+{
+public:      
+    string fileName;
+    csvHandler(string filename)
+    {
+        fileName = filename;
+    }
+    // Function to read from CSV file
+    fstream readCSV() {
+        fstream file(fileName);
+        if (!file.is_open()) {
+            cout << "Could not open the file!" << endl;
+        }
+        cout << "File opened successfully!" << endl;
+        return file;
+    }
+    void writeCSV(string dataLine="") {
+        fstream file(fileName,ios::app);
+        if (!file.is_open()) {
+            cout << "Could not open the file!" << endl;
+        }
+        file << "\n" + dataLine << endl;
+        cout << "Data written successfully!" << endl;
+        file.close();
+    }
+};
+
+
+//Class to store material properties
+class Material
+{
 public:
     int index;
     string name;
     float yield;
     float density;
+    vector<Material> materials;
 
     
-    material(int i = 0, string n = "", float y = 0, float d = 0) {
-        index = i;
+    Material(string n = "", string y = "0.0", string d = "0.0") {
         name = n;
-        yield = y;
-        density = d;
+        yield = stof(y);
+        density = stof(d);
     }
+
+    // Function to read materials from CSV file and return a vector of Material objects
+    vector<Material> materialList()
+    {
+        csvHandler handler("./Data/Processed/materials.csv");
+        fstream file = handler.readCSV();
+        string dataLine;
+        getline(file, dataLine); // Skip header line
+        while (getline(file, dataLine)) {
+            stringstream ss(dataLine);
+            string index, n, y, d;
+            getline(ss, index, ',');  // Read index but not needed
+            getline(ss, n, ',');
+            getline(ss, y, ',');
+            getline(ss, d, ',');
+            materials.push_back(Material(n, y, d));
+        }
+        file.close();
+        return materials;
+    };
+
+    // add Material to the CSV file/Vector
+    void addMaterial(Material material)
+    {
+        csvHandler handler("./Data/Processed/materials.csv");
+        string dataLine = to_string(materialList().size()) + "," + material.name + "," + to_string(material.yield) + "," + to_string(material.density);
+        handler.writeCSV(dataLine);
+        materials.push_back(material);
+    }
+
 };
 
-vector<material> loadData(string filename) {
-    vector<material> dataList;
-    ifstream file(filename);
-    string line;
-
-   
-    getline(file, line); 
-
-    while (getline(file, line)) {
-        
-
-        stringstream ss(line);
-        string n, i, y, d;
-
-        getline(ss, i, ',');
-        getline(ss, n, ',');
-        getline(ss, y, ',');
-        getline(ss, d, ',');
-
-        
-        
-        dataList.push_back(material(stoi(i), n, stof(y), stof(d)));
-        
-    }
-    file.close();
-    return dataList;
+void test()
+{
+    // vector<Material> materials= Material().materialList();
+    // for (Material material : materials) {
+    //     cout << "Index: " << material.index << ", Name: " << material.name 
+    //          << ", Yield: " << material.yield << ", Density: " << material.density << endl;
+    // }
+    //Material().addMaterial(Material("dgdg", "454.0", "55.0"));
+    //cout<<Material().materialList()[0].name<<endl;
 }
 
-void addData(string filename, material newMat) {
-    ofstream file(filename, ios::app); 
-    if (file.is_open()) {
-        file << newMat.index << "," << newMat.name << "," << newMat.yield << "," << newMat.density << endl;
-        file.close();
-    }
-}
-
-int main() {
-   
-    string path = "Data/Processed/materials.csv"; 
-    vector<material> allMat = loadData(path);
-    
-    int x;
-    cout << "1. Choose a material \n2. Add a material\nSelection: ";
-    cin >> x;
-
-    if (x == 1) {
-        
-        if (allMat.empty()) {
-            cout << "No data found or file error." << endl;
-            return 0;
-        }
-
-        int y;
-        cout << "Enter list index (0 to " << allMat.size()-1 << "): ";
-        cin >> y;
-
-        cout << allMat[y].name << " | " << allMat[y].yield << " | " << allMat[y].density << endl;
-       
-    } 
-    else if (x == 2) {
-        int i; 
-        string n; 
-        float y, d;
-        cout << "Name: "; cin >> n;
-        cout << "Yield: "; cin >> y;
-        cout << "Density: "; cin >> d;
-
-        material newMat(allMat.size(), n, y, d);
-        addData(path, newMat);
-        cout << "Material added!" << endl;
-    }
-
+int main()
+{
+    Material().addMaterial(Material("dgdg", "454.0", "55.0"));
     return 0;
+
 }
