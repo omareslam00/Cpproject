8ad1aa2bfad2732abd21b76e4c66b1ee64674e4d
EbrahimElTehewy <ebrahemeltehewy@gmail.com>
2026-04-21 23:51:20 +0200

    add getMaterials, fix numbers input

--- Mod1.cpp
+++ Mod1.cpp
@@ -30,7 +30,7 @@ public:
         if (!file.is_open()) {
             cout << "Could not open the file!" << endl;
         }
-        file << "\n" + dataLine << endl;
+        file <<dataLine << endl;
         cout << "Data written successfully!" << endl;
         file.close();
     }
@@ -41,7 +41,6 @@ public:
 class Material
 {
 public:
-    int index;
     string name;
     float yield;
     float density;
@@ -75,12 +74,62 @@ public:
     };
 
     // add Material to the CSV file/Vector
-    void addMaterial(Material material)
+    void addMaterial()
     {
+        string n, y, d;
+        cout << "Enter material name | yield | density: ";
+        while (true)
+        {
+            try
+            {
+                cin >> n >> y >> d;
+                stof(y);
+                stof(d);
+                break; // Exit loop if conversion is successful
+            }
+            catch(exception e)
+            {
+                cout << "Invalid input. \nPlease enter valid yield and density values: " ;
+            }
+            
+        }
+
         csvHandler handler("./Data/Processed/materials.csv");
-        string dataLine = to_string(materialList().size()) + "," + material.name + "," + to_string(material.yield) + "," + to_string(material.density);
+        string dataLine = to_string(materialList().size()) + "," + n + "," + y + "," + d;
         handler.writeCSV(dataLine);
-        materials.push_back(material);
+        materials.push_back(Material(n, y, d));
+    }
+
+    // display materials and get user selection
+    Material getMaterial() {
+        materials = materialList(); // Load materials from CSV file
+        cout << "Available Materials:" << endl;
+        cout << "Index | Name | Yield | Density" << endl;
+        for(int i=0;i<materials.size();i++){
+            cout << i << " | " << materials[i].name << " | " << materials[i].yield <<" | "<< materials[i].density << endl;
+        }
+        string i; // store index as string to be converted to int after validation
+        cout << "enter index of the required material:  ";
+
+        // Loop until a valid index is entered
+        while (true)
+        {
+            try
+            {
+                cin>>i;
+                int index = stoi(i); // Convert string input to integer
+                if (index < 0 || index >= materials.size()) {
+                    throw out_of_range("Index out of range");  // Check if index is within valid range
+                }
+                return materials[index];
+            }
+            catch(exception e)  //catch any error (invalid input or out of range)
+            {
+                cout << "Invalid input. \nPlease enter a valid index: " ;
+            }
+            
+        }
+
     }
 
 };
@@ -89,29 +138,33 @@ public:
 // demonstrate the usage of Material class and CSV handling
 void doc()
 {
-    // vector<Material> materials= Material().materialList();  // Get the list of materials from the CSV file
+    // vector<Material> materials= Material().materialList();  //// Get the list of materials from the CSV file
 
-    //how to get each material's properties
+    ////how to get each material's properties
 
     // for (Material material : materials) {
     //     cout << "Index: " << material.index << ", Name: " << material.name 
     //          << ", Yield: " << material.yield << ", Density: " << material.density << endl;
     // }
 
-    // add a new material to the CSV file and the materials vector
+    //// add a new material to the CSV file and the materials vector
 
-    //Material().addMaterial(Material("dgdg", "454.0", "55.0"));
+    //Material().addMaterial();
 
 
-    // ger the value of specific material property
+    //// get the value of specific material property
 
     //cout<<Material().materialList()[0].name<<endl;
+
+
+    //// get user selection of material and display its properties
+    // Material material = Material().getMaterial();
+    // cout << "Selected Material: " << material.name << ", Yield: " << material.yield << ", Density: " << material.density << endl;
+
 }
 
-
 int main()
-{
-    doc();
+{ 
+    Material().addMaterial();
     return 0;
-
 }
